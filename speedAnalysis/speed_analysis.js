3bd2d274b2dea46b778ee843ce56b5cc2789bcb6
abed <freazer122@gmail.com>
2025-02-15 04:16:01 -0500

    added functionality and fixed a bug givven by the code from coursera

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -7,11 +7,14 @@ function startTest() {
     // Reset results and timer
     document.getElementById("output").innerHTML = "";
     startTime = new Date().getTime();
-
+    //allow writing text
+    document.getElementById("userInput").readOnly = false;
+    document.getElementById("userInput").placeholder="Start typing when ready";
     // Change button text and functionality
     var button = document.getElementById("btn");
     button.innerHTML = "End Test";
     button.onclick = endTest;
+
 }
 function endTest() {
     endTime = new Date().getTime();
@@ -27,7 +30,7 @@ function endTest() {
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
         return word !== "";
     }).length;
-
+    var total_length=userTypedText.length
     var wpm = 0; // Default value
 
     if (timeElapsed !== 0 && !isNaN(typedWords)) {
@@ -37,6 +40,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: "+ total_length + "</p>"+
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
@@ -45,4 +49,7 @@ function endTest() {
     var button = document.getElementById("btn");
     button.innerHTML = "Start Test";
     button.onclick = startTest;
+    // change the typing text box to empty
+    var user_text=document.getElementById("userInput");
+    user_text.value='';
 }
